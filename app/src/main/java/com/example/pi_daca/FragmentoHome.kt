11d9ff6565ca9ce02f3b72c5e028ff5cbfd6b79e
gabriel.acassiano <gabriel.adlungue@gmail.com>
2022-04-13 20:27:31 -0300

    HomePage

---
+++ app/src/main/java/com/example/pi_daca/FragmentoHome.kt
@@ -0,0 +1,23 @@
+package com.example.pi_daca
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pi_daca.databinding.FragmentoHomeBinding
+
+
+class FragmentoHome : Fragment() {
+    lateinit var binding: FragmentoHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = FragmentoHomeBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/pi_daca/TesteViews.kt
@@ -0,0 +1,11 @@
+package com.example.pi_daca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TesteViews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teste_views)
+    }
+}
