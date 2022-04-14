956882e976d6562145b84b6e85e5739aad193d91
Gabriel Adelungue <40636838+gabriel-adelungue@users.noreply.github.com>
2022-04-14 17:16:27 -0300

    teste 14/04

--- app/src/main/java/com/example/pi_daca/FragmentoHome.kt
+++ app/src/main/java/com/example/pi_daca/FragmentoHome.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.pi_daca.databinding.FragmentoHomeBinding
 
+// teste versionamento
 
 class FragmentoHome : Fragment() {
     lateinit var binding: FragmentoHomeBinding
