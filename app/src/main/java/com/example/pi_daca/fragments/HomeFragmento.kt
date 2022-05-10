bdf59ba105793cf6bf751d14f53029ca6f4dec65
cacoabud <cacoabud@gmail.com>
2022-05-10 16:05:53 -0300

    Adição dos Cards, Singletons e Estruturação dos Fragmentos e Atividades Relacionados

--- app/src/main/java/com/example/pi_daca/fragments/HomeFragmento.kt
+++ app/src/main/java/com/example/pi_daca/fragments/HomeFragmento.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.pi_daca.R
 import com.example.pi_daca.data.ReportsCardObject
+import com.example.pi_daca.data.reportCardData
 import com.example.pi_daca.databinding.ActivityFaqBinding
 import com.example.pi_daca.databinding.CardItemBinding
 import com.example.pi_daca.databinding.HomeFragmentoBinding
@@ -16,6 +17,21 @@ import com.example.pi_daca.databinding.ReportCardBinding
 class HomeFragmento : Fragment() {
     lateinit var binding: HomeFragmentoBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = HomeFragmentoBinding.inflate(inflater)
+
+        fun updateCard(){
+            binding.containerHome.removeAllViews()
+
+            ReportsCardObject.listReports.forEach{
+                val card = ReportCardBinding.inflate(layoutInflater)
+
+                card.titleReport.text = it.title
+                card.descReport.text = it.desc
+
+                binding.containerHome.addView(card.root)
+            }
+        }
 
         return binding.root
     }
